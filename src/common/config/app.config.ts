0309117d8089b86aae83785fb49415e3fb0f4ac6
JSL107 <juneseok0107@naver.com>
2025-09-07 14:16:58 +0900

    fix : 포트변경 3000 => 8001

--- src/common/config/app.config.ts
+++ src/common/config/app.config.ts
@@ -1,5 +1,5 @@
 import { registerAs } from '@nestjs/config';
 
 export const appConfig = registerAs('app', () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: parseInt(process.env.PORT, 10),
 }));
